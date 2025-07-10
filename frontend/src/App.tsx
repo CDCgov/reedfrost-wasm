dd7d644a45e2cd5c10049ae98d7cdbcfcb9588e6
Scott Olesen <ulp7@cdc.gov>
2025-07-10 16:54:05 -0400

    Comments and attempts to callback

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { run } from "@wasm/reedfrost";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Slider from "@mui/material/Slider";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import { Button } from "@mui/material";
+// some of these have brackets, and some not. How predictable is that?
+// It has something to do with import paths
 import Typography from "@mui/material/Typography";
 import { BarChart } from "@mui/x-charts/BarChart";
 
@@ -15,8 +17,10 @@ type MySliderProps = {
   max: number;
   step: number;
   valueFormatter?: (value: number) => string;
+  callback?: () => void;
 };
 
+// This feels like a component?
 function MySlider({
   name,
   value,
@@ -25,6 +29,7 @@ function MySlider({
   max,
   step,
   valueFormatter = (x) => x.toString(),
+  callback = () => {},
 }: MySliderProps) {
   return (
     <Box>
@@ -34,7 +39,10 @@ function MySlider({
           <Slider
             aria-labelledby={`${name}-slider`}
             value={value}
-            onChange={(_, newValue) => setterFunc(newValue)}
+            onChange={(_, newValue) => {
+              setterFunc(newValue);
+              callback();
+            }}
             min={min}
             max={max}
             step={step}
@@ -48,13 +56,20 @@ function MySlider({
   );
 }
 
+// Is this idiomatic? To make a derived/child component, like a subclass?
 type PercentSliderProps = {
   name: string;
   value: number;
   setterFunc: (value: number) => void;
+  callback?: () => void;
 };
 
-function PercentSlider({ name, value, setterFunc }: PercentSliderProps) {
+function PercentSlider({
+  name,
+  value,
+  setterFunc,
+  callback,
+}: PercentSliderProps) {
   return (
     <MySlider
       name={name}
@@ -63,11 +78,14 @@ function PercentSlider({ name, value, setterFunc }: PercentSliderProps) {
       min={0.0}
       max={1.0}
       step={0.01}
+      // This doesn't work like I expect
       valueFormatter={(x) => `${x * 100}%`}
+      callback={callback}
     />
   );
 }
 
+// This likely isn't a "component," but it is some kind of view field?
 function renderResult(result: object[] | null) {
   if (result === null) {
     return <>No result</>;
@@ -90,13 +108,15 @@ function renderResult(result: object[] | null) {
   }
 }
 
+// Is this a "component"? Something bigger?
 function simShower() {
   const [result, setResult] = useState<object[] | null>(null);
   const [s0, setS0] = useState<number>(10);
   const [i0, setI0] = useState<number>(1);
   const [prob, setProb] = useState<number>(0.1);
 
-  function runClick() {
+  // Run simulations with the current parameters
+  function runResult() {
     let newResult = [];
     for (let k = 0; k <= s0; k++) {
       newResult.push({ k: k, pmf: run(k, s0, i0, prob) });
@@ -104,14 +124,21 @@ function simShower() {
     setResult(newResult);
   }
 
-  function resetClick() {
-    setResult(null);
-  }
+  // Run the simulation when the component mounts
+  useEffect(() => {
+    runResult();
+  }, []);
 
   return (
     <>
       <Typography variant="h2">Parameters</Typography>
-      <PercentSlider name="Probability" value={prob} setterFunc={setProb} />
+      <PercentSlider
+        name="Probability"
+        value={prob}
+        setterFunc={setProb}
+        // This hangs/crashes?
+        // callback={runResult}
+      />
       <MySlider
         name="Initial S"
         value={s0}
@@ -119,6 +146,7 @@ function simShower() {
         min={1}
         max={50}
         step={1}
+        // callback={runResult}
       />
       <MySlider
         name="Initial I"
@@ -127,12 +155,10 @@ function simShower() {
         min={1}
         max={10}
         step={1}
+        // callback={runResult}
       />
-      <Button variant="outlined" onClick={runClick}>
+      <Button variant="contained" onClick={runResult}>
         Run simulation
-      </Button>
-      <Button variant="outlined" onClick={resetClick}>
-        Reset
       </Button>
       <Typography variant="h2">Result</Typography>
       {renderResult(result)}
