f1b315eb9040c05c1a8b872f4b6f4c3abcc233b6
Scott Olesen <ulp7@cdc.gov>
2025-07-10 15:50:21 -0400

    make cleaner sliders

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,36 @@
 import { run } from "@wasm/reedfrost";
 import { useState } from "react";
 import Slider from "@mui/material/Slider";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { Button } from "@mui/material";
 
-function mySlider(
-  name: string,
-  oldValue: number,
-  setterFunc: (value: number) => void,
-  min: number,
-  max: number,
-  step: number,
-) {
-  const handleChange = (_: Event, value: number) => {
-    setterFunc(value);
-  };
+type MySliderProps = {
+  name: string;
+  value: number;
+  setterFunc: (value: number) => void;
+  min: number;
+  max: number;
+  step: number;
+};
 
+function MySlider({ name, value, setterFunc, min, max, step }: MySliderProps) {
   return (
-    <>
-      <Slider
-        value={oldValue}
-        onChange={handleChange}
-        min={min}
-        max={max}
-        step={step}
-      />
-      <div>
-        {name}: {oldValue}
-      </div>
-    </>
+    <Box>
+      {name}
+      <Grid container spacing={2}>
+        <Grid size="grow">
+          <Slider
+            value={value}
+            onChange={(_, newValue) => setterFunc(newValue)}
+            min={min}
+            max={max}
+            step={step}
+          />
+        </Grid>
+        <Grid>{value}</Grid>
+      </Grid>
+    </Box>
   );
 }
 
@@ -53,11 +57,11 @@ function simShower() {
   const [prob, setProb] = useState<number>(0.1);
 
   function runClick() {
-    let result = new Array();
+    let newResult = [];
     for (let k = 0; k <= s0; k++) {
-      result.push(run(k, s0, i0, prob));
+      newResult.push(run(k, s0, i0, prob));
     }
-    setResult(result);
+    setResult(newResult);
   }
 
   function resetClick() {
@@ -66,11 +70,36 @@ function simShower() {
 
   return (
     <>
-      {mySlider("Probability", prob, setProb, 0.0, 1.0, 0.01)}
-      {mySlider("Initial S", s0, setS0, 1, 50, 1)}
-      {mySlider("Initial I", i0, setI0, 1, 10, 1)}
-      <button onClick={runClick}>Run simulation</button>
-      <button onClick={resetClick}>Reset</button>
+      <MySlider
+        name="Probability"
+        value={prob}
+        setterFunc={setProb}
+        min={0.0}
+        max={1.0}
+        step={0.01}
+      />
+      <MySlider
+        name="Initial S"
+        value={s0}
+        setterFunc={setS0}
+        min={1}
+        max={50}
+        step={1}
+      />
+      <MySlider
+        name="Initial I"
+        value={i0}
+        setterFunc={setI0}
+        min={1}
+        max={10}
+        step={1}
+      />
+      <Button variant="outlined" onClick={runClick}>
+        Run simulation
+      </Button>
+      <Button variant="outlined" onClick={resetClick}>
+        Reset
+      </Button>
       <h2>Result</h2>
       {renderResult(result)}
     </>
